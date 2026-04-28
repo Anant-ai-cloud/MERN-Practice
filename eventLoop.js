66d6e79217e1bf7c8cdfe8806ee15f34a2113273
Anant <anantpathak564@gmail.com>
2026-04-28 20:31:45 +0530

    Javascript is going on

--- eventLoop.js
+++ eventLoop.js
@@ -53,17 +53,76 @@
 //     console.log("Correct")
 // }
 
-let m = "monday"
+// let m = "monday"
 
-switch(m){
+// switch(m){
 
-    case "tuesday":
-    console.log("tuesday");
-    break;
+//     case "tuesday":
+//     console.log("tuesday");
+//     break;
 
-    case "monday" :
-        console.log("monday")
-        break;
-}
+//     case "monday" :
+//         console.log("monday")
+//         break;
+// }
 
+//for...in used to get keys
 
+// for(let i in [10,20]){
+//     console.log(i)
+// }
+
+//for...of used to get values
+
+// for(let i of [10,20]){
+//     console.log(i)
+// }
+
+// console.log(a)
+// var a = 10
+
+// console.log(b)   //for let and const temporal deadzone so here you will get refrence error
+// let b = 3
+
+// const greet = function(){  //function expression can't be hoist
+    // console.log("Hey")
+// }
+// greet()
+
+//CRUD operation on Arrays
+
+// let arr = [10, 20]  //create
+// console.log(arr)  //read
+// arr[1] = 30 //update
+// arr.splice(1,1) //delete  (delete arr[1] bad practice leaves empty space)
+// console.log(arr)
+
+// let arr = [1,2,3,4]
+
+// const newArr= arr.map((e)=> e*2)  //map
+// const newArr = arr.filter((e)=> e%2 === 0)  //filter
+// const newArr = arr.reduce((total, n)=> total+n, 0)  //reduce
+
+// let arr = [{id:1}, {id:2}, {id: 3}]
+// console.log(arr.find(e=> e.id === 2))  //first match
+
+// arr.forEach(e=> console.log(e))
+
+//logical method
+
+// console.log(arr.some(e=> e>3)) //atleast one element should follow condition
+// console.log(arr.every(e=> e>3)) //every element should follow condition
+
+//flatting the array
+
+//flat(depth)
+// let arr = [1,[2,[3]]]
+// const newArr = arr.flat(2)
+// console.log(newArr)
+
+let arr = [2,5,1]
+
+// const newArr = arr.flatMap(e=> [e, e+1])
+// console.log(newArr)
+const newArr = arr.sort((a,b)=> a+b)
+console.log(newArr)
